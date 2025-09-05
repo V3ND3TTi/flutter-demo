d49e51c6aef2b79ecc5c5ca9696e24fe4a4680ed
Devon Vendetti <v3nd3tti@icloud.com>
2025-09-05 00:07:54 -0700

    initial commit for new flutter project

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+// I need to import a helper library from flutter to get content on the screen
+
+// Define a 'main' function to run when our app starts
+
+// Create a new teext widget to show some text on the screen
+
+// Take that widget and get it on the screen
